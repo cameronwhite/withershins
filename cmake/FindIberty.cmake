0657860da6544aaf61b0a588b8e3738e5bba428e
Cameron White <cameronwhite91@gmail.com>
2014-12-30 18:21:51 -0500

    Improve how libiberty.a is searched for on OSX.

--- cmake/FindIberty.cmake
+++ cmake/FindIberty.cmake
@@ -2,7 +2,10 @@ find_path(IBERTY_INCLUDE_DIRS libiberty.h
     PATHS /usr/include /usr/local/include
     PATH_SUFFIXES libiberty
 )
-find_library(IBERTY_LIBRARIES iberty)
+find_library(IBERTY_LIBRARIES
+    libiberty.a iberty
+    PATH_SUFFIXES x86_64
+)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Iberty DEFAULT_MSG IBERTY_LIBRARIES IBERTY_INCLUDE_DIRS)
