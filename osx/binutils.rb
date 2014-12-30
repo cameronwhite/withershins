4b15f5e1044cc3e7d0c3f8a371ecb5e2161733ed
Cameron White <cameronwhite91@gmail.com>
2014-12-29 23:42:36 -0500

    Fix how the patch is read.

--- osx/binutils.rb
+++ osx/binutils.rb
@@ -6,15 +6,13 @@ class Binutils < Formula
   mirror 'http://ftp.gnu.org/gnu/binutils/binutils-2.24.tar.gz'
   sha1 '1b2bc33003f4997d38fadaa276c1f0321329ec56'
 
-  def patches
-    # [PATCH] libiberty: fix --enable-install-libiberty flag
-    # fixed the --disable-install-libiberty behavior, but it also
-    # added a bug where the enable path never works because the initial clear
-    # of target_header_dir wasn't deleted.  So we end up initializing properly
-    # at the top only to reset it at the end all the time.
-    #  http://gcc.gnu.org/ml/gcc-patches/2014-01/msg00213.html
-    '20_all_libiberty-install.patch'
-  end
+  # [PATCH] libiberty: fix --enable-install-libiberty flag
+  # fixed the --disable-install-libiberty behavior, but it also
+  # added a bug where the enable path never works because the initial clear
+  # of target_header_dir wasn't deleted.  So we end up initializing properly
+  # at the top only to reset it at the end all the time.
+  #  http://gcc.gnu.org/ml/gcc-patches/2014-01/msg00213.html
+  patch IO.read('20_all_libiberty-install.patch')
 
   def install
     system "./configure", "--disable-debug",
