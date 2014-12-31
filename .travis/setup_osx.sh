bba35a75e3d301209af5ed1ddf4abcbcdfdd85a6
Cameron White <cameronwhite91@gmail.com>
2014-12-31 14:00:51 -0500

    Remove debugging code and update comments.

--- .travis/setup_osx.sh
+++ .travis/setup_osx.sh
@@ -2,9 +2,7 @@
 set -e # Exit immediately if an error occurs.
 set -x # Echo commands.
 
-pwd
-
 brew update
-# Install Qt, gettext, and binutils.
+# Install gettext and binutils.
 brew install gettext osx/binutils.rb
 brew link --force gettext
