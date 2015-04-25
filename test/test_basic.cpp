9edd95288dff5d4259e329d74630dc3a58696e3c
Cameron White <cameronwhite91@gmail.com>
2015-04-25 15:17:05 -0400

    Fix unused function warnings on OSX.

--- test/test_basic.cpp
+++ test/test_basic.cpp
@@ -14,6 +14,11 @@ void foo()
     bar();
 }
 
+#if defined(_WIN32) || defined(WITHERSHINS_ENABLE_LIBBFD)
+#define ENABLE_FILE_INFO
+#endif
+
+#ifdef ENABLE_FILE_INFO
 static bool ends_with(const std::string &a, const std::string &b)
 {
     if (a.length() >= b.length())
@@ -21,6 +26,7 @@ static bool ends_with(const std::string &a, const std::string &b)
     else
         return false;
 }
+#endif
 
 void baz()
 {
@@ -33,7 +39,7 @@ void baz()
     REQUIRE(frames[1].function_name() == "bar");
     REQUIRE(frames[2].function_name() == "foo");
 
-#if defined(_WIN32) || defined(WITHERSHINS_ENABLE_LIBBFD)
+#ifdef ENABLE_FILE_INFO
     REQUIRE(ends_with(frames[0].file_name(), "test_basic.cpp"));
     REQUIRE(frames[1].line_number() == 10);
     REQUIRE(frames[2].line_number() == 15);
